0b5cc5b6833f038f146ec53f3734b71d4dce1936
Justin DuJardin <justin@dujardinconsulting.com>
2015-12-19 14:37:40 -0800

    refactor(icon): bind class using host object in directive annotation
     - remove use of DOM

--- ng2-material/components/icon/icon.ts
+++ ng2-material/components/icon/icon.ts
@@ -1,12 +1,10 @@
 import {Directive, ElementRef} from 'angular2/core';
-import {DOM} from 'angular2/src/platform/dom/dom_adapter';
-
 
 @Directive({
-  selector: '[md-icon], .md-icon'
+  selector: '[md-icon], .md-icon',
+  host: {
+    '[class.material-icons]': 'true'
+  }
 })
 export class MdIcon {
-  constructor(element: ElementRef) {
-    DOM.addClass(element.nativeElement, 'material-icons');
-  }
 }
