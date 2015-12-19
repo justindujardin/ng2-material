d58dd4e7f675ebb3c5c950ce701f14ea476b1f18
Justin DuJardin <justin@dujardinconsulting.com>
2015-12-19 11:32:34 -0800

    chore: fix issue with deployed site and rxjs config

--- config.js
+++ config.js
@@ -5,13 +5,9 @@ System.config({
     },
     'examples': {
       defaultExtension: 'js'
-    },
-    'rxjs': {
-      defaultExtension: 'js'
     }
   },
   map: {
-    "rxjs": "node_modules/rxjs",
     "angular2/*" : "node_modules/angular2/*"
   },
   baseURL: './'
