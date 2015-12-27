b22c49eff88c16776f317c66d67a57f01d40abaa
Jaroslav Řehořka <jaroslav.rehorka@gmail.com>
2015-12-28 00:43:20 +0100

    progress_circular example sass added

--- examples/components/progress_circular/basic_usage.ts
+++ examples/components/progress_circular/basic_usage.ts
@@ -2,14 +2,17 @@ import {View, Component} from 'angular2/core';
 import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 
 @Component({selector: 'progress-circular-basic-usage'})
-@View({templateUrl: 'examples/components/progress_circular/basic_usage.html', directives: [MATERIAL_DIRECTIVES]})
+@View({
+  templateUrl: 'examples/components/progress_circular/basic_usage.html',
+  styleUrls: ['examples/components/progress_circular/basic_usage.css'],
+  directives: [MATERIAL_DIRECTIVES]
+})
 export default class ProgressCircularBasicUsage {
   public determinateValue:number = 30;
   public deactivated:boolean = false;
   public mode:string;
 
   constructor() {
-
     // Iterate every 100ms, non-stop
     setInterval(() => {
       this.determinateValue += 1;
