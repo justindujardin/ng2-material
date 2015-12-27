33fceec06d18e43be097ad01bc9a8aa4d39734e3
Jaroslav Řehořka <jaroslav.rehorka@gmail.com>
2015-12-27 02:27:41 +0100

    progress_circurlar: usage added

---
+++ examples/components/progress_circular/basic_usage.ts
@@ -0,0 +1,27 @@
+import {View, Component} from 'angular2/core';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
+
+@Component({selector: 'progress-circular-basic-usage'})
+@View({templateUrl: 'examples/components/progress_circular/basic_usage.html', directives: [MATERIAL_DIRECTIVES]})
+export default class ProgressCircularBasicUsage {
+  public determinateValue:number = 30;
+  public deactivated:boolean = false;
+  public mode:string;
+
+  constructor() {
+
+    // Iterate every 100ms, non-stop
+    setInterval(() => {
+      this.determinateValue += 1;
+
+      if (this.determinateValue > 100) {
+        this.determinateValue = 30;
+      }
+    }, 100, 0, true);
+  }
+
+  toogleActivation() {
+    this.deactivated = !this.deactivated;
+    this.mode = this.deactivated ? 'indeterminate' : '';
+  }
+}
