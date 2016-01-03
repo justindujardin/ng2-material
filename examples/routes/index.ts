ec3524063fa4a5d5e12e27996e2206a0546998e4
Justin DuJardin <justin@dujardinconsulting.com>
2016-01-02 17:11:16 -0800

    feat(examples): add getting started instructions to main page

--- examples/routes/index.ts
+++ examples/routes/index.ts
@@ -7,27 +7,11 @@ import {NavigationService} from "../services/navigation";
 import {MATERIAL_DIRECTIVES} from "ng2-material/all";
 import {ROUTER_DIRECTIVES} from "angular2/router";
 import {DOM} from "angular2/src/platform/dom/dom_adapter";
+import {Highlight} from "../highlight";
 
 @Component({
-  template: `
-    <h1 class="examples-title">Getting Started</h1>
-    <p class="examples-intro">
-      There are many examples linked from this page. Here's the <a href="coverage/" target="_blank">test coverage</a> report, and a
-      <a href="http://plnkr.co/edit/CnDUjVufVnevluFOBvdD?p=preview" target="_blank">plunkr template</a> to get you going.
-      <span>
-        You can find the source code for this project on <a href="https://github.com/justindujardin/ng2-material">Github</a>.
-      </span>
-
-    </p>
-
-
-    <nav class="examples-toc">
-      <h1>Components</h1>
-      <ul>
-        <li *ngFor="#value of components"><a [routerLink]="['Component', {id: value.id}]">{{value.name}}</a></li>
-      </ul>
-    </nav>`,
-  directives: [ROUTER_DIRECTIVES, MATERIAL_DIRECTIVES]
+  templateUrl: 'examples/routes/index.html',
+  directives: [ROUTER_DIRECTIVES, Highlight, MATERIAL_DIRECTIVES]
 })
 export class IndexPage implements OnInit {
   public components: IComponentMeta[] = [];
