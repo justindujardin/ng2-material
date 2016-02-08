5e1759ccf110df80af657c0b5f9aa40fd37612fc
Justin DuJardin <justin@dujardinconsulting.com>
2016-02-07 16:07:17 -0800

    fix(build): make ie name shim compatible with coverage code
     - instrumentation was breaking stackoverflow regexp.
     - remove testing debug code

--- karma.ie.shims.js
+++ karma.ie.shims.js
@@ -1,12 +1,9 @@
-console.log("------------------------------ INCLUDING IE SHIMS");
-
 // function.name (all IE)
 /*! @source http://stackoverflow.com/questions/6903762/function-name-not-supported-in-ie*/
 if (!Object.hasOwnProperty('name')) {
-  console.log("------------------------------ Applying name shim");
   Object.defineProperty(Function.prototype, 'name', {
     get: function() {
-      var matches = this.toString().match(/^\s*function\s*(\S*)\s*\(/);
+      var matches = this.toString().match(/^\s*function\s*(\S[^\(]*)\s*\(/);
       var name = matches && matches.length > 1 ? matches[1] : "";
       // For better performance only parse once, and then cache the
       // result through a new accessor for repeated access.
