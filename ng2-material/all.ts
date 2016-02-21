9c8bbebfa6bc6032e3cb52e321e05d7853c7cf92
N. Schipper <n.schippper@twensoc.nl>
2016-02-21 14:50:53 +0100

    Fixed reference error: https://github.com/justindujardin/ng2-material/issues/70 The reason i had to use the filesGlob for this is readable here: https://www.npmjs.com/package/grunt-ts#tsconfigas the filesGlob overwrites the files array within the tsconfig.build.json.

--- ng2-material/all.ts
+++ ng2-material/all.ts
@@ -1,5 +1,3 @@
-///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
-
 import {CONST_EXPR, Type} from 'angular2/src/facade/lang';
 
 import {MdAnchor, MdButton} from './components/button/button';
