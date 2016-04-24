bfe3310ec6897477b16556aef0014f915de08007
Justin DuJardin <justin@dujardinconsulting.com>
2016-04-24 15:36:34 -0700

    chore: stash wip chips updates
     - deletable can be bound on the chips component
     - styles for deletable and non-deletable chips look OK
     - use onpush change detection for better performance
     - TODO: How to bind a data source to the component? I feel like I'm fighting with the data service here.

--- examples/components/chips/basic_usage.ts
+++ examples/components/chips/basic_usage.ts
@@ -1,7 +1,6 @@
-import {Component, ViewChild, AfterContentInit} from "angular2/core";
+import {Component} from "angular2/core";
 import {MATERIAL_DIRECTIVES} from "ng2-material/all";
 import {FORM_DIRECTIVES} from "angular2/common";
-import {MdChips} from "../../../ng2-material/components/chips/chips";
 
 @Component({
   selector: 'chips-basic-usage',
@@ -9,12 +8,6 @@ import {MdChips} from "../../../ng2-material/components/chips/chips";
   styleUrls: ['examples/components/chips/basic_usage.css'],
   directives: [MATERIAL_DIRECTIVES, FORM_DIRECTIVES]
 })
-export default class ChipsBasicUsage implements AfterContentInit {
-  ngAfterContentInit(): any {
-    console.log(this.child);
-  }
-
-  deletable: boolean = false;
-
-  @ViewChild(MdChips) child: MdChips;
+export default class ChipsBasicUsage {
+  deletable: boolean = true;
 }
