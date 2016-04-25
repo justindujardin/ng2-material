9e6d257f676a0f3095464bc1080d106169ac6766
Alexander Zeiher <alexander@zeiher.se>
2016-04-25 04:59:27 +0200

    Fix
    forgot to add a file

---
+++ ng2-material/core/util/uuid.ts
@@ -0,0 +1,21 @@
+
+/* jshint bitwise:false, node:true */
+/* tslint:disable:no-bitwise no-var-keyword typedef */
+
+// taken from TodoMVC
+// taken again from https://raw.githubusercontent.com/ng-book/angular2-rxjs-chat/master/app/ts/util/uuid.ts
+export function uuid() {
+  var i, random;
+  var result = '';
+
+  for (i = 0; i < 32; i++) {
+    random = Math.random() * 16 | 0;
+    if (i === 8 || i === 12 || i === 16 || i === 20) {
+      result += '-';
+    }
+    result += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random))
+      .toString(16);
+  }
+
+  return result;
+}
