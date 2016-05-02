b8ad272da362c5d10550048611e420443d7ffd3a
Justin DuJardin <justin@dujardinconsulting.com>
2016-05-01 17:32:32 -0700

    feat(material2): add material2 core as a peerDependency
     - here we go :rocket:

--- config.js
+++ config.js
@@ -1,11 +1,19 @@
 System.config({
   packages: {
+    '@angular2-material/core': {
+      format: 'cjs',
+      defaultExtension: 'js',
+      main: 'core.js'
+    },
     'ng2-material': {
       defaultExtension: 'js'
     },
     'examples': {
       defaultExtension: 'js'
     }
+  },
+  map: {
+    '@angular2-material/core': 'node_modules/@angular2-material/core'
   },
   baseURL: './'
 });
