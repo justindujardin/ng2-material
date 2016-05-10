2d67fff3fc3227567d959217a4a8000f47a595eb
Justin DuJardin <justin@dujardinconsulting.com>
2016-05-09 19:55:39 -0700

    chore: fix index highlight code blocks

--- modules/docs/src/app/+index/index.component.ts
+++ modules/docs/src/app/+index/index.component.ts
@@ -2,16 +2,19 @@ import {Component, OnInit} from '@angular/core';
 import {MATERIAL_DIRECTIVES} from 'ng2-material';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 import {ComponentsService} from '../shared';
+import {Highlight} from '../components/highlight';
 
 @Component({
   moduleId: module.id,
   selector: 'app-index',
   templateUrl: 'index.component.html',
   styleUrls: ['index.component.css'],
-  directives: [MATERIAL_DIRECTIVES, ROUTER_DIRECTIVES]
+  directives: [MATERIAL_DIRECTIVES, ROUTER_DIRECTIVES, Highlight]
 })
 export class IndexComponent implements OnInit {
-  constructor(private components: ComponentsService) {}
+  constructor(private components: ComponentsService) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 }
