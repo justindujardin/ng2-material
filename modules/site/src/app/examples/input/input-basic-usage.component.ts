a3543908a9a72ebe341d6a456b0530fa2d796cba
Vincent Palita <vincent@blackalbum.com>
2016-07-20 06:41:32 +0200

    remove commented model

--- modules/site/src/app/examples/input/input-basic-usage.component.ts
+++ modules/site/src/app/examples/input/input-basic-usage.component.ts
@@ -14,31 +14,6 @@ import {REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, Validators, FormBuilde
   providers: [QueryList]
 })
 export class InputBasicUsageComponent {
-  /**
-   * Unused model
-   */
-  // user = {
-  //   title: 'Developer',
-  //   email: 'ipsum@lorem.com',
-  //   firstName: '',
-  //   lastName: '',
-  //   company: 'Google',
-  //   address: '1600 Amphitheatre Pkwy',
-  //   address2: '',
-  //   city: 'Mountain View',
-  //   state: 'CA',
-  //   biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-  //   postalCode: '94043'
-  // };
-  // states = [
-  //   'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL',
-  //   'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT',
-  //   'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',
-  //   'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
-  // ].map(function (state) {
-  //   return {abbrev: state};
-  // });
-
 
   form:FormGroup;
   postalCode:FormControl;
